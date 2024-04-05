3376e67c37930fb261dd3fd8435d64f93c764ad2
RazhPL <antekwojnowicz@interia.pl>
2024-04-05 20:36:07 +0200

    Player Tileset

--- obj/Debug/net6.0/Cosmic Labirynth.AssemblyInfo.cs
+++ obj/Debug/net6.0/Cosmic Labirynth.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cosmic Labirynth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7d3f537595010fd7e7a998b8702a6827c3182f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cosmic Labirynth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cosmic Labirynth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
